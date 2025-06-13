ae9adc3d36982f23f2a5a792ad93adb04ae29769
yaro1214 <15934071030@163.com>
2025-06-13 11:00:56 +0800

    update scripts

--- slow-fast-sampling/scripts/dream_run_mbpp.sh
+++ slow-fast-sampling/scripts/dream_run_mbpp.sh
@@ -4,7 +4,7 @@ model="Dream-org/Dream-v0-Base-7B"
 
 export HF_ALLOW_CODE_EVAL=1
 
-ACCEL_CONFIG="accelerate_config.yaml"
+ACCEL_CONFIG="eval_config.yaml"
 MAIN_PORT="29510" 
 
 echo "Starting evaluation for mbpp"
